f7f3f3fc45faa8c02d62411c23ceacf09fecb88f
David Rowell <david.rowell@cd-adapco.com>
2017-01-24 11:55:27 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+DevOps Coding Challenge
+=======================
+
+We asked our junior developer to write a program to read from two
+web services and report on the status of all the feature branches.
+
+He made a good start, but decided to leave before finishing his work.
+Please take a look at his code and finish up anything you see that
+needs work.
+
+To complete this challenge:
+
+* Obtain a machine with java 8+ and maven 3+ installed.
+* Create a github account if you do not already have one.
+* Clone this repository.
+* Make changes to the source files.  Keep an eye out for bugs and fix them,
+and optimizations you can make and make them.  We expect all unit tests to pass.
+* Commit your changes, push to your repository, and create a pull request.
+* You should feel free to do a code review!  State why you did things the way you
+did, how you would improve the code if you had more time, etc.  We love good ideas,
+so let's hear them.
+
+Notes
+=====
+Expect that the buildStatuses and featureBranches are pretty rigid, i.e. no changing these files to fit
+your unit tests!
---
+++ src/test/java/devops/FeatureBranchReportTest.java
@@ -0,0 +1,119 @@
+package devops;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for Feature Branch Report App.
+ */
+public class FeatureBranchReportTest {
+
+    private static final int TEST_PORT = 9999;
+    private Server server = null;
+
+    @Before
+    public void setUp() throws Exception {
+        server = new Server(TEST_PORT);
+        new WebAppContext(server, "src/test/webapp/", "/");
+
+        // Start Server
+        server.start();
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        // Stop Server
+        server.stop();
+    }
+    
+    /**
+     * Test the report
+     * @throws IOException 
+     * @throws MalformedURLException 
+     */
+    @Test
+    public void testReport() throws MalformedURLException, IOException {
+        
+        final FeatureBranchReport app = new FeatureBranchReport() {
+            public Collection<String> getFeatureBranches() {
+                final Collection<String> branches = new ArrayList<String>();
+                
+                branches.add("feature/abc/TEST-123");
+                branches.add("feature/abc/TEST-124");
+                branches.add("feature/xyz/TEST-125");
+                branches.add("feature/xyz/TEST-126");
+                branches.add("feature/xyz/TEST-127");
+                
+                return branches;
+            }
+
+            public Map<String, String> getCIBuildStatuses() {
+                final Map<String, String> statuses = new TreeMap<String, String>();
+                
+                statuses.put("feature-abc-TEST-122", "SUCCESS");
+                statuses.put("feature-abc-TEST-123", "SUCCESS");
+                statuses.put("feature-abc-TEST-124", "FAILURE");
+                
+                return statuses;
+            }
+        };
+        
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        
+        app.printFeatureBranchBuildReport(new PrintStream(baos));
+        
+        final String report = baos.toString("UTF-8");
+        
+        assertTrue(report.contains("feature/abc/TEST-123"));
+        // Could use some more tests
+    }
+    
+    @Test
+    public void testGetFeatureBranches() throws MalformedURLException, IOException {
+        final FeatureBranchReport app = new FeatureBranchReport() {
+            public String getScmBaseUrl() {
+                return "http://localhost:" + TEST_PORT + "/scm";
+            }
+        };
+        
+        final Collection<String> branches = app.getFeatureBranches();
+        assertEquals(7, branches.size());
+        assertTrue(branches.contains("feature/abc/TEST-121"));
+        assertTrue(branches.contains("feature/abc/TEST-122"));
+        assertTrue(branches.contains("feature/abc/TEST-123"));
+        assertTrue(branches.contains("feature/abc/TEST-124"));
+        assertTrue(branches.contains("feature/abc/TEST-125"));
+        assertTrue(branches.contains("feature/abc/TEST-126"));
+        assertTrue(branches.contains("feature/abc/TEST-127"));
+    }
+
+    @Test
+    public void testGetBuildStatuses() throws MalformedURLException, IOException {
+        final FeatureBranchReport app = new FeatureBranchReport() {
+            public String getCIBaseUrl() {
+                return "http://localhost:" + TEST_PORT + "/ci";
+            }
+        };
+        
+        final Map<String, String> statuses = app.getCIBuildStatuses();
+        assertEquals(3, statuses.keySet().size());
+        assertTrue(statuses.get("feature-abc-TEST-122").equals("SUCCESS"));
+        assertTrue(statuses.get("feature-abc-TEST-123").equals("SUCCESS"));
+        assertTrue(statuses.get("feature-abc-TEST-124").equals("FAILURE"));
+    }
+}
